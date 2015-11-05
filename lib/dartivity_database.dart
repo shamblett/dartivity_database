4ff7a2863bf117873f2ed8add369da3c74c042e2
Steve Hamblett <steve.hamblett@linux.com>
2015-11-05 10:46:00 +0000

    Initial structure

---
+++ lib/dartivity_database.dart
@@ -0,0 +1,14 @@
+/*
+ * Package : dartivity_database
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 05/011/2015
+ * Copyright :  S.Hamblett 2015
+ */
+
+library dartivity_database;
+
+import 'dart:io';
+
+import 'package:wilt/wilt.dart';
+
+part 'src/dartivity_database.dart';
---
+++ lib/src/dartivity_database.dart
@@ -0,0 +1,11 @@
+/*
+ * Package : dartivity_database
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 05/011/2015
+ * Copyright :  S.Hamblett 2015
+ */
+
+part of dartivity_database;
+
+class DartivityDatabase {
+}
