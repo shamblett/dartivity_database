8e08cc1f030d65cfc06dfccd9ce82028507bbd60
steve <steve.hamblett@linux.com>
2015-11-12 12:57:43 +0000

    Rename the resource files

--- lib/dartivity_database.dart
+++ lib/dartivity_database.dart
@@ -13,11 +13,12 @@ import 'package:wilt/wilt.dart';
 import 'package:wilt/wilt_server_client.dart';
 import 'package:json_object/json_object.dart' as json;
 import 'package:crypto/crypto.dart' show MD5, CryptoUtils;
+
 part 'dartivity_resource_database.dart';
 
-part 'dartivity_database_iotivity_resource.dart';
+part 'dartivity_iotivity_resource.dart';
 
-part 'dartivity_database_resource.dart';
+part 'dartivity_resource.dart';
 part 'src/dartivity_database.dart';
 
 /// Library globals
