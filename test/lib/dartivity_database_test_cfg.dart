4bb0e77da6532d0b923260baeade75442c0a6e51
steve <steve.hamblett@linux.com>
2015-11-26 13:26:00 +0000

    Add sanitised test cfg file, ready for 0.1.0 release

---
+++ test/lib/dartivity_database_test_cfg.dart
@@ -0,0 +1,6 @@
+library dartivity_resource_database_test;
+
+String clientId = 'dell-base-d5dcb860-3ad8-5329-ab54-5c3c9c5f2242%625';
+String hostname = 'db.dartivity.com';
+String user = 'xxx';
+String password = 'yyyy';
